7ef4da4eb8904b465b2e4bb40bbd71593e1a5d3d
Ejmin Keshishian <ejminkeshishi@gmail.com>
2017-02-21 10:42:05 +0330

    Initial Commit

---
+++ First-ios-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First-ios-app
+//
+//  Created by Ejmin Keshishi on 2/21/17.
+//  Copyright © 2017 Ejmin Keshishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
